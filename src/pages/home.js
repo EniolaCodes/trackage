d1ec88113e4932ce9369493bfc4bc75ff68cd813
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-04-09 21:08:53 +0100

    add logo to the landing page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,25 @@
 import Link from "next/link";
-// import Image from "next/image"
+import Image from "next/image"
 
 const LandingPage = () => {
   return (
-    <div className="bg-gradient-to-b from-blue-500 to-blue-700 text-white min-h-screen flex flex-col">
-  <div className="relative flex-grow">
-    <header className="container mx-auto py-8 flex justify-between items-center">
-      <h1 className="text-4xl font-bold">Trackage</h1>
-      <nav className="space-x-4">
-        <Link href="/adminlayout" className="text-2xl text-white hover:underline">Admin</Link>
-        <Link href="/userlayout" className="text-2xl text-white hover:underline">Member</Link>
-      </nav>
-    </header>
-    <main className="container mx-auto flex flex-col items-center justify-center flex-grow">
-      <h2 className="text-5xl font-bold text-center mb-8">Real-Time Package Monitoring</h2>
-      <p className="text-lg text-center mb-12">Track your packages seamlessly with Trackage's real-time monitoring system. Say goodbye to lost packages and delivery uncertainties.</p>
-      <Link href="/signup" className="bg-white text-blue-500 px-8 py-3 rounded-md shadow-lg hover:bg-blue-100 transition duration-300 ease-in-out">Get Started</Link>
-    </main>
-  </div>
-  <footer className="bg-gray-900 text-white py-4 text-center">
-    <p>&copy; {new Date().getFullYear()} Trackage. All rights reserved.</p>
-  </footer>
-</div>
-
+    <div className="bg-gradient-to-b from-blue-500 to-blue-700 text-white min-h-screen flex flex-col relative">
+    <Image src={'/assets/map.jpg'} alt='map' layout="fill" objectFit="cover" />
+    <div className="relative flex-grow">
+      <header className="container mx-auto py-8 flex justify-between items-center">
+      <Image src={'/assets/Frame 18.png'} alt='trackage logo' width={200} height={200} />
+        <nav className="space-x-4">
+          <Link href="/adminlayout" className="text-2xl text-blue-400 font-bold hover:underline">Admin</Link>
+          <Link href="/userlayout" className="text-2xl text-blue-400 font-bold hover:underline">Member</Link>
+        </nav>
+      </header>
+      <main className="container mx-auto flex flex-col items-center justify-center flex-grow">
+        <h2 className="text-5xl font-bold text-center mb-8">Real-Time Package Monitoring</h2>
+        <p className="text-3xl text-center mb-12">Track your packages seamlessly with Trackage's real-time monitoring system. Say goodbye to lost packages and delivery uncertainties.</p>
+        <Link href="/signup" className="bg-white text-blue-500 px-8 py-3 rounded-md shadow-lg hover:bg-blue-100 transition duration-300 ease-in-out">Get Started</Link>
+      </main>
+    </div>
+  </div> 
   );
 }
 
